a07638ce79064a739f01792d0c63b1ca13bda88a
Khushi Patel <168915185+khushipatel1510@users.noreply.github.com>
2024-09-11 23:55:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,50 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
-import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
-import { v2 as cloudinary } from "cloudinary";
-import { app, server } from "./socket/socket.js";
-import job from "./cron/cron.js";
-import cors from 'cors';
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./db/connectDB.js');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoutes.js');
+const postRoutes = require('./routes/postRoutes.js');
+const messageRoutes = require('./routes/messageRoutes.js');
 
-
+const { v2: cloudinary } = require('cloudinary');
+const { app, server } = require('./socket/socket.js');
+const job = require('./cron/cron.js');
+const cors = require('cors');
 
 dotenv.config();
-
 connectDB();
 job.start();
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 app.use(cors({
-	origin: 'https://mern-azure-web-app-c5frbpfca7fgfhgv.centralindia-01.azurewebsites.net/api', // frontend URL
-	credentials: true
-  }));
+  origin: 'https://mern-azure-web-app-c5frbpfca7fgfhgv.centralindia-01.azurewebsites.net/api', // frontend URL
+  credentials: true
+}));
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Middlewares
-app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/messages", messageRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/messages', messageRoutes);
 
-// http://localhost:5000 => backend,frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
 }
 
-
-
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
